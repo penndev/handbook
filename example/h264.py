8920b28201c27526a166810c15f79b9fa9e5f126
penndev <penndev@users.noreply.github.com>
2024-09-23 15:44:38 +0000

    deploy: 835878a70993c1c9054d61d03e1d305b4389c3a2

--- example/h264.py
+++ example/h264.py
@@ -2,22 +2,38 @@
 class H264: 
     list = []
     def setFile(self,filename):
-        '根据flv文件进行tag分析与提取'
         with open(filename, 'rb') as f:
             self.hex = bytearray(f.read())
         hexCount = len(self.hex)
         readCount = 0
-        nal = bytearray()
+        sliceStart = 0  
         while hexCount > readCount:
-            if self.hex[readCount] == 0 and self.hex[readCount+1] == 0 :
-                if self.hex[readCount+2] == 1 or (self.hex[readCount+2] == 0 and self.hex[readCount+3] == 1): 
-                    self.list.append(nal)
-                    nal = bytearray()
-            nal.append(self.hex[readCount])
-            readCount += 1
-        self.list = self.list[1:]
+            if self.hex[readCount] != 0 or self.hex[readCount+1] != 0:
+                readCount += 1
+                continue
+            
+            startCodeLen = 0
+            if self.hex[readCount+2] == 0 :
+                if self.hex[readCount+3] == 1 :
+                    startCodeLen = 4
+            elif self.hex[readCount+2] == 1 :
+                startCodeLen = 3
+
+            if startCodeLen == 0:
+                readCount += 1
+            else:
+                if readCount > 0:
+                    self.list.append(self.hex[sliceStart:readCount])
+                readCount += startCodeLen
+                sliceStart = readCount
+        # 最后一个结尾
+        self.list.append(self.hex[sliceStart:readCount])
+        
+            
 
 if __name__ == "__main__":
     v = H264()
     v.setFile("./vfile/output.h264")
-    print(v.list)
+    with open("./vfile/output.bin", "wb") as file:
+        for element in v.list:
+            file.write(element)
