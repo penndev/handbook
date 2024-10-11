faff594617387886bb837e4797f1fdcc6bdaf23e
penndev <pennilessfor@gmail.com>
2024-10-11 20:02:29 +0800

    debug h264

--- Media/h26x_example/h264_define.py
+++ Media/h26x_example/h264_define.py
@@ -51,6 +51,8 @@ class BitStream():
     def __init__(self, hex:bytearray) -> None:
         self.hex = hex
         self.position = 0
+    
+    # 读取原始字节流基础函数，移动指针
     def read_bits(self, n) -> int:
         '从字节流 返回n位无符号整数 大字节序列'
         value = 0  # 用于存储读取的值
@@ -65,6 +67,8 @@ class BitStream():
             self.position += bits_to_read  # 更新位置和剩余要读取的位数
             n -= bits_to_read
         return value
+    
+    # 哥伦布编码
     def read_ue(self) -> int:
         '''解析 H.264 中的 ue(v) (无符号指数哥伦布编码0阶)'''
         leading_zeros = 0
@@ -72,14 +76,46 @@ class BitStream():
             leading_zeros += 1
         if leading_zeros == 0: # 2. 读取对应数量的位
             return 0  # 特殊情况，前导 0 个数为 0 时，值为 0
-        return (1 << leading_zeros) - 1 + self.read_bits(leading_zeros)
+        return (1 << leading_zeros) - 1 + self.read_bits(leading_zeros)    
     def read_se(self) -> int:
         code_num = self.read_ue()
         if code_num % 2 == 0:
             return -(code_num // 2) 
         else:
             return code_num // 2 + 1 
+    def read_me(self, chroma_format_idc):
+        ''' 9.1.2 进行数值对照表 对本数据进行说明。 根据codeNum进行表 9-4 对照返回数据。
+            sps.chroma_format_idc  != 0 返回a表 
+            sps.chroma_format_idc  == 0 返回b表 
+        '''
+        codeNum = self.read_ue()
+        raise('not support me(v)')
+
+    # 
+    def read_ae(self):
+        ''' CABAC解析过程 \n
+            sps. entropy_coding_mode_flag = 1 才会调用本过程
+        '''
+        pStateIdx
+        valMPS1
+        raise('not support ae(v)')
+    def read_te(self):
+        raise('not support te(v)') 
+
+    # 逻辑验证
     def more_rbsp_data(self):
         return self.position / 8 + 2 > len(self.hex)
+    def more_rbsp_trailing_data(self):
+        return self.position < len(self.hex)
     def byte_aligned(self):
         return self.position % 8 == 0
+    
+    def cabac_mb_type(self, sliceType):
+        # table 9-25
+        if sliceType == SliceType.I:
+            ctxIdxOffset = 3
+            maxBinIdxCtx = 6
+        else:
+            raise("mb_type not support " + str(sliceType))
+        ctxIdxInc = condTermFlagA + condTermFlagB
+        return ctxIdxInc
